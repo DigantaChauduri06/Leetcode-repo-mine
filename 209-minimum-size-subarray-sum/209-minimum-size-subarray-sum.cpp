ebcea099160ebcf44083aff5a98aa8c43e864c89
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-05-21 09:51:51 +0530

    Time: 8 ms (76.83%), Space: 10.6 MB (60.91%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     int minSubArrayLen(int k, vector<int>& nums) {
-        unordered_map<int, int> mpp;
+        int i = 0, j = 0;
+        int n = nums.size() - 1;
         int cur = 0;
-        int i = 0;
         int mn = INT_MAX;
-        int n = nums.size();
-        while (i < n) {
-            cur += nums[i];
-            if (cur == k) {
-                mn = min(mn, (i + 1));
+        while (i <= n) {
+            cur += nums[i++];
+            while (cur >= k) {
+                mn = min(mn, (i - j));
+                cur -= nums[j++];
             }
-            if (nums[i] == k) return 1;
-            int rem = (cur - k);
-            if (mpp.find(rem) != mpp.end()) {
-                int idx = mpp[rem];
-                mn = min(mn, (i - idx));
-            }
-            if (cur > k) {
-                int j = 0;
-                int curj = cur;
-                while (j < n && (curj) > k) {
-                    mn = min(mn, (i - j + 1));
-                    curj -= nums[j++];
-                }
-            }
-            mpp[cur] = i;
-            i++;
         }
-        return (mn == INT_MAX) ? 0 : mn;
+        return mn == INT_MAX ? 0 : mn;
     }
 };
