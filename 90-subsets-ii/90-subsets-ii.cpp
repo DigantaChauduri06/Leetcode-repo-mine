c24c81761949886ec63c838968bd1788effb1795
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-05-27 08:19:11 +0530

    Time: 8 ms (37.33%), Space: 7.4 MB (89.42%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> st;
+        vector<vector<int>> ans;
         vector<int> temp;
         sort(begin(nums), end(nums));
-        fun(nums, st, temp);
-        return vector<vector<int>>(st.begin(), st.end());
+        fun(nums, ans, temp);
+        return ans;
     }
 private:
-        void fun(vector<int> &nums,set<vector<int>> &ans, vector<int> &arr, int i = 0) {
+        void fun(vector<int> &nums,vector<vector<int>> &ans, vector<int> &arr, int i = 0) {
         if (i == nums.size()) {
-            // sort(begin(arr),end(arr));
-            ans.insert(arr);
+            ans.push_back(arr);
             return;
         }
         // pick
@@ -19,6 +18,8 @@ private:
         fun(nums, ans, arr, i+1);
         arr.pop_back();
         // not - pick
+        // remove duplicate
+        while ((i+1) < nums.size() && nums[i] == nums[i+1]) i++;
         fun(nums, ans, arr, i+1);
     }
 };
