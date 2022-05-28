85341d2f128343715795632ab282589f6fd74807
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-05-28 10:01:03 +0530

    Time: 397 ms (63.65%), Space: 8.3 MB (7.51%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -6,24 +6,24 @@ public:
         for (int i = 0;i < m;i++) {
             for (int j = 0;j < n;j++) {
                 if (board[i][j] == word[0]) {
-                    if (dfs(board, word,i,j,m,n)) return true;
+                    vector<vector<bool>> seen(m, vector<bool>(n, false));
+                    if (dfs(board, word,i,j,m,n, seen)) return true;
                 }
             }
         }
         return false;
     }
 private:
-    bool dfs(vector<vector<char>> &board, string &word, int i, int j, int m, int n) {
+    bool dfs(vector<vector<char>> &board, string &word, int i, int j, int m, int n, vector<vector<bool>> &seen) {
         if (word.size() == 0) return true;
-        if (i >= m || j >= n || i < 0 || j < 0 || board[i][j] != word[0]) return false;
-        char cur = board[i][j];
-        board[i][j] = '*';
+        if (i >= m || j >= n || i < 0 || j < 0 || board[i][j] != word[0] || seen[i][j]) return false;
+        seen[i][j] = true;
         string rem = word.substr(1);
-        bool left = dfs(board, rem, i-1, j, m,n);
-        bool right = dfs(board, rem, i+1, j, m,n);
-        bool up = dfs(board, rem, i, j-1, m,n);
-        bool down = dfs(board, rem, i, j+1, m,n);
-        board[i][j] = cur;
+        bool left = dfs(board, rem, i-1, j, m,n, seen);
+        bool right = dfs(board, rem, i+1, j, m,n, seen);
+        bool up = dfs(board, rem, i, j-1, m,n, seen);
+        bool down = dfs(board, rem, i, j+1, m,n, seen);
+        seen[i][j] = false;
         return left || right || up || down;
         
     }
