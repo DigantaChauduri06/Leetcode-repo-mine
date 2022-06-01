61688303de0ecbc7469f211cb2eb7fb391b24f41
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-01 18:42:45 +0530

    Time: 551 ms (18.79%), Space: 73.6 MB (30.19%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,23 +1,20 @@
 class Solution {
-    typedef pair<float, vector<int>> prii;
+typedef pair<int, vector<int>> pvi;
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& p, int k) {                                                       
-        auto comp = [](prii &a, prii &b) {
-            // max-heap
-            return a.first <= b.first;
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        auto comp = [](pvi &a, pvi &b) {
+          return a.first <= b.first;  
         };
-        priority_queue<prii,vector<prii>, decltype(comp)> pq(comp);
-        for (int i = 0;i < p.size();i++) {
-            long long a = p[i][0] * p[i][0];
-            long long b = p[i][1] * p[i][1];
-            float diff = sqrt((a+b));
-            pq.push({diff, p[i]});
-            if (pq.size() > (k)) {
-                pq.pop();
-            }
-        }
         vector<vector<int>> ans;
-        while(!pq.empty()) {
+        priority_queue<pvi, vector<pvi>, decltype(comp)> pq(comp);
+        for (int i = 0;i < points.size();i++) {
+            int a = points[i][0];
+            int b = points[i][1];
+            int dis = a*a + b*b;
+            pq.push({dis, points[i]});
+            if (pq.size() > k) pq.pop();
+        }
+        while (!pq.empty()) {
             ans.push_back(pq.top().second);
             pq.pop();
         }
