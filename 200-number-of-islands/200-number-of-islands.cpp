95ca0f30bc0eb6256f7c63e560040d6120e672ac
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-01 11:39:36 +0530

    Time: 63 ms (25.51%), Space: 18.2 MB (14.97%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -8,21 +8,42 @@ public:
             for (int j = 0;j < n;j++) {
                 if (grid[i][j] == '1') {
                     cnt++;
-                    dfs(grid, i, j, m, n);
+                    bfs(grid, i, j, m, n);
                 }
             }
         }
-        return cnt;
+        return cnt; 
     }
 private:
-    void dfs(vector<vector<char>> &grid, int i, int j, int m, int n) {
-        if (i == m || j == n || i < 0 || j < 0 || grid[i][j] != '1') 
-            return;
-        grid[i][j] = '2';
-        dfs(grid, i - 1, j, m, n);
-        dfs(grid, i + 1, j, m, n);
-        dfs(grid, i, j - 1, m, n);
-        dfs(grid, i, j + 1, m, n);
-        return;
+    bool isInRange(int i, int j, int m, int n) {
+        return (i >= 0 && j >= 0 && i < m && j < n);
+    }
+    void bfs(vector<vector<char>> &grid, int i, int j, int m, int n) {
+        if (i < 0 || j < 0 || i >= m || j >= n) return;
+        queue<pair<int, int>> q;
+        q.push({i,j});
+        while (q.size() != 0) {
+            auto f = q.front();
+            q.pop();
+            int x = f.first;
+            int y = f.second; 
+            grid[x][y] = '0';
+            if (isInRange(x-1,y,m,n) && grid[x-1][y] == '1'){
+                    grid[x-1][y] = '0';
+                    q.push({x-1, y});
+            }
+            if (isInRange(x+1,y,m,n) && grid[x+1][y] == '1'){
+                    grid[x+1][y] = '0';
+                    q.push({x+1, y});
+            }
+            if (isInRange(x,y-1,m,n) && grid[x][y-1] == '1'){
+                    grid[x][y-1] = '0';
+                    q.push({x, y-1});
+            }
+            if (isInRange(x,y+1,m,n) && grid[x][y+1] == '1'){
+                grid[x][y+1] = '0';
+                q.push({x, y+1});
+            }
+        }
     }
 };
