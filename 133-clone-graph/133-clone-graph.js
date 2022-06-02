ea1d100110577d3ebd98bb563a82b2ff51fc0e20
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-02 19:57:48 +0530

    Time: 97 ms (31.16%), Space: 43.8 MB (70.99%) - LeetHub

--- 133-clone-graph/133-clone-graph.js
+++ 133-clone-graph/133-clone-graph.js
@@ -14,9 +14,9 @@ var cloneGraph = function(node) {
     if (!node) return node;
     const mpp = new Map()
     const dfs = (node) => {
-        if (mpp.has(node.val)) return mpp.get(node.val);
+        if (mpp.has(node)) return mpp.get(node);
         const nn = new Node(node.val);
-        mpp.set(node.val, nn);
+        mpp.set(node, nn);
         // Triversing the neighbors
         for (const nei of node.neighbors) {
             const cur = dfs(nei);
