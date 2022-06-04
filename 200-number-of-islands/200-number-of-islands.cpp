77e11896e9e5ba45dd7f47b1c5ed543b372a36e0
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-04 19:24:15 +0530

    Time: 66 ms (23.29%), Space: 18.2 MB (18.28%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,5 +1,6 @@
 class Solution {
     int ROW, COL;
+    vector<pair<int, int>> dirs{{0, -1},{-1, 0}, {1, 0}, {0, 1}};
 public:
     int numIslands(vector<vector<char>>& G) {
         ROW = size(G), COL = size(G[0]);
@@ -8,22 +9,32 @@ public:
             for (int j = 0;j < COL;j++) {
                 if (G[i][j] == '1') {
                     count += 1;
-                    dfs(G, i, j);
+                    bfs(G, i, j);
                 }
             }
         }
         return count;
     }
 private:
-    void dfs(vector<vector<char>> &G, int i, int j) {
-        if (valid(G, i, j)) {
-            G[i][j] = '0';
-            // call in all dirs
-            dfs(G, i+1, j);
-            dfs(G, i-1, j);
-            dfs(G, i, j+1);
-            dfs(G, i, j-1);
-        } else return;
+    void bfs(vector<vector<char>> &G, int i, int j) {
+        queue<pair<int, int>> q;
+        q.push(make_pair(i, j));
+        G[i][j] = '0';
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+                for (int i = 0;i < 4;i++) {
+                    int r = row + dirs[i].first;
+                    int c = col + dirs[i].second;
+                    if (!valid(G, r, c)) continue;
+                    q.push(make_pair(r, c));
+                    G[r][c] = '0';
+                }
+            }
+        }
     }
     bool valid(vector<vector<char>> &G, int i, int j) {
         return i >= 0 and
