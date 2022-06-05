ecc22fa3d09f656509e56520f36dc025c3a44d7f
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-05 23:13:04 +0530

    Time: 133 ms (27.22%), Space: 48.2 MB (99.71%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,23 +1,12 @@
 class Solution {
-    vector<int> dp;
 public:
-    bool canJump(vector<int>& nums, int i = 0) {
-        dp.assign(size(nums)+1, -1);
-        return func(nums, i);
-    }
-private:
-    bool func(vector<int> &nums, int i) {
-        if (i >= size(nums) - 1) return true;
-        int curJump = nums[i];
-        if (dp[i] != -1) return dp[i];
-        if (curJump == 0) return dp[i] = false;
-        for (int j = 1;j <= curJump;j++) {
-            int jump = i+j;
-            if (jump > size(nums) -1) return dp[i] = false;
-            if (func(nums, jump)) {
-                return dp[i] = true;
+    bool canJump(vector<int>& nums) {
+        int goal = size(nums) - 1;
+        for (int i = size(nums)-2;i >= 0;i--) {
+            if ((nums[i] + i) >= goal) {
+                goal = i;
             }
         }
-        return dp[i] = false;
+        return goal == 0;
     }
 };
