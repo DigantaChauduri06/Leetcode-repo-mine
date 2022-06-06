698d8ac5b50ac3047f77a4a38bd6b596c5786e5a
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-06 19:43:10 +0530

    Time: 61 ms (8.22%), Space: 16.6 MB (7.55%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    unordered_map<int, list<int>> adj;
+    vector<int> ans;
+    unordered_set<int> seen, st;
+public:
+    vector<int> findOrder(int nc, vector<vector<int>>& preq) {
+        for (auto &p : preq) {
+            adj[p[0]].push_back(p[1]);
+        }
+        for (int i = 0;i < nc;i++) {
+            if (!topo_sort(i)) {
+                return {};
+            }
+        }
+        return ans;
+    }
+private:
+    bool topo_sort(int sv) {
+        if (seen.find(sv) != seen.end()) return false;
+        if (adj[sv].empty()) {
+            if (st.find(sv) == st.end()){
+                ans.push_back(sv);
+                st.insert(sv);
+            }
+            return true;
+        }
+        seen.insert(sv);
+        for (auto &node : adj[sv]) {
+            if (!topo_sort(node)) {
+                return false;
+            }
+        }
+        seen.erase(sv);
+        if (st.find(sv) == st.end()) {
+            ans.push_back(sv);
+            st.insert(sv);
+        }
+        adj[sv].clear();
+        return true;
+    }
+};
