fc931e3aebbbbbfd4ff6f4d2cf625b56b144fdd0
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-06 12:47:33 +0530

    Time: 33 ms (42.27%), Space: 15.8 MB (13.05%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    map<int, list<int>> adj;
+    unordered_set<int> st;
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& P) {
+        for (auto &p : P) {
+            adj[p[0]].push_back(p[1]);
+        }
+        for (int i = 0;i < numCourses;i++) {
+            if (!dfs(i)) return false;
+        }
+        return true;
+    }
+private:
+    bool dfs(int sv) {
+        if (st.find(sv) != st.end()) return false;
+        if (adj[sv].empty()) return true;
+        
+        st.insert(sv);
+        for (auto &node : adj[sv]) {
+            if (!dfs(node)) return false;
+        }
+        adj[sv].clear();
+        st.erase(sv);
+        return true;
+    }
+};
