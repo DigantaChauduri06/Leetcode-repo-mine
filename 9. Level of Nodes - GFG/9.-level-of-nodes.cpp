fdef5df55f4ebd28cc835a05c63fb802daf769e9
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-13 19:57:34 +0530

    Added solution - LeetHub

---
+++ 9. Level of Nodes - GFG/9.-level-of-nodes.cpp
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution
+{
+	public:
+	//Function to find the level of node X.
+	int nodeLevel(int V, vector<int> adj[], int X) {
+	    // code here
+	    int lvl = 0;
+	    queue<int> q;
+	    q.push(0);
+	    unordered_set<int> st;
+	    st.insert(0);
+	    while (!q.empty()) {
+	        int size = q.size();
+	        while (size--)  {
+               int f = q.front();
+               q.pop();
+               if (f == X) 
+                    return lvl;
+               for (auto &node : adj[f]) {
+                   if (st.find(node) == st.end()) {
+                       st.insert(node);
+                       q.push(node);
+                   }
+               }
+	        }
+           lvl++;
+	    }
+	    return -1;
+	}
+};
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E, X;
+    	cin >> V >> E;
+
+    	vector<int> adj[V];
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    		adj[v].push_back(u);
+    	}
+    	cin>>X;
+
+    	Solution obj;
+    	cout << obj.nodeLevel(V, adj, X) << "\n";
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
