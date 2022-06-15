ff8240e00a837d3e791b98dddc85bfe2be777331
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-15 10:19:21 +0530

    Time: 15 ms (88.97%), Space: 17 MB (74.59%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& I, vector<int>& NI) {
-        
-        I.push_back(NI);
-        sort(begin(I), end(I));
-        return mergeIntervals(I);
-    }
-private:
-    vector<vector<int>> mergeIntervals(vector<vector<int>> &I) {
-        vector<vector<int>> ans;
-        ans.push_back(I[0]);
-        for (int i = 1;i < size(I);i++) {
-            if (ans.back()[1] >= I[i][0]) {
-                ans.back()[1] = max(ans.back()[1], I[i][1]);
-            }
-            else {
-                ans.push_back(I[i]);
-            }
+        int i = 0;
+        int n = size(I);
+        vector<vector<int>> result;
+        while (i < n && NI[0] > I[i][1]) {
+            result.push_back(I[i]);
+            i++;
         }
-        return ans;
+        while (i < n && NI[1] >= I[i][0]) {
+            NI[0] = min(I[i][0], NI[0]);
+            NI[1] = max(I[i][1], NI[1]);
+            i++;
+        }
+        result.push_back(NI);
+        while (i < n) {
+            result.push_back(I[i]);
+            i++;
+        }
+        return result;
+        
     }
 };
+/*
+    3 steps:
 
+    1. push all the intervals before the starting of New                    Intervals
+    2. merge the new Interval
+    3. add all the remaining intervals in the result
+*/
 /*
     [[1,2],[3,5],[6,7],[8,10],[12,16]]
-    [4,8]
+    [19, 200]
 
 */
