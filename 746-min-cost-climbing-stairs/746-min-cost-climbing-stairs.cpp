79f4c5de68efc6df1ee9e9d485fb82b35e88e6bb
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-24 23:10:19 +0530

    Time: 3 ms (96.84%), Space: 13.6 MB (96.38%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -3,11 +3,22 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         n = size(cost);
-        vector<int> dp1(n+1);
-        return fun(cost, dp1);
+        return fun(cost);
     }
 private:
-    int fun(vector<int> &cost, vector<int> &dp) {
+    int fun(vector<int> &cost) {
+        if (n == 0) return 0;
+        if (n <= 2) return n == 1 ? cost[0] : min(cost[0], cost[1]);
+        int prev1 = cost[1], prev2 = cost[0];
+        for (int j = 2;j < n;j++) {
+            int cur = min(prev1, prev2) + cost[j];
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return min(prev1, prev2);
+    }
+};
+/*
         if (n == 0) return 0;
         if (n <= 2) return n == 1 ? cost[0] : min(cost[0], cost[1]);
         dp[0] = cost[0];
@@ -16,9 +27,8 @@ private:
             dp[j] = min(dp[j-1], dp[j-2]) + cost[j];
         } 
         return min(dp[n-2], dp[n-1]);
-    }
-};
-/*
+
+
         if (i == n - 1 || i == n - 2) return cost[i];
         int one = fun(cost, i+1);
         int two = fun(cost, i+2);
