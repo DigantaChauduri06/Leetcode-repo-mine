0276702ffc600749bed9dc0fd14b6ffe727b105f
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-24 19:40:43 +0530

    Time: 2 ms (41.55%), Space: 6.2 MB (14.87%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -7,12 +7,8 @@ public:
     }
 private:
     int fun(int n) {
-        if (n == 0) 
-            return 0;
-        if (n == 1)
+        if (n == 0 || n == 1) 
             return 1;
-        if (n == 2)
-            return 2;
         if (dp[n] != -1) 
             return dp[n];
         return dp[n] =  fun(n-1) + fun(n-2);
