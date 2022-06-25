3c2300a533e904a48a3783007ba9ab01c5a6e36d
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-25 09:39:45 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (24.65%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,21 +1,32 @@
 class Solution {
     int n;
-    vector<int> cache;
+    vector<int> dp;
 public:
     int rob(vector<int>& nums) {
         n = size(nums);
-        cache.assign(n+1, -1);
-        return fun(nums, 0);      
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        dp.assign(n+1,-1);
+        return fun(nums);      
     }
 private:
-    int fun(vector<int> &nums, int i) {
+    int fun(vector<int> &nums) {
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 1;i < n;i++) {
+            dp[i+1] = max(dp[i], dp[i-1] + nums[i]);
+        }
+        return dp[n];
+    }
+};
+
+/*
         if (i >= n) 
             return 0;
         if (cache[i] != -1) 
             return cache[i];
         return cache[i] = max(fun(nums, i+1), nums[i]+fun(nums, i+2));
-    }
-};
 
-//[2,1,1,2]
+
+*/
 
