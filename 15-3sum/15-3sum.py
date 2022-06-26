c0c02e8ced12d43abb357f3351420be0213bf7c8
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-26 20:57:48 +0530

    Time: 1709 ms (36.43%), Space: 18.2 MB (41.47%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,19 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int], tar = 0) -> List[List[int]]:
-        temp = set()
-        # res = []
-        for i in range(len(nums)):
-            track = {}
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
             # 2 sum
-            for j in range(i+1, len(nums)):
-                cur = nums[i] + nums[j]
-                diff = tar - cur
-                if track.get(diff, "None") != "None":
-                    # print([nums[i], nums[j], nums[track[diff]]])
-                    found = sorted([nums[i], nums[j], nums[track[diff]]])
-                    temp.add(tuple(found))
+            # rem = tar - nums[i]
+            if i >= 1 and nums[i-1] == nums[i]:
+                continue
+            si, ei = i+1, len(nums)-1
+            while si < ei:
+                cur = nums[i] + nums[si] + nums[ei]
+                if cur == tar:
+                    found = [nums[i], nums[si], nums[ei]]
+                    res.append(found)
+                    while si<ei and nums[si]==nums[si+1]:
+                        si+=1
+                    while si<ei and nums[ei]==nums[ei-1]:
+                        ei-=1
+                    si+=1
+                    ei-=1
+                elif cur > tar:
+                    ei -= 1
                 else:
-                    track[nums[j]] = j
-        return list(temp)
-        
+                    si += 1
+        return res
+    
+    
 # [-1,0,1,2,-1,-4]
+# [-4,-1,-1,0,2,4]
