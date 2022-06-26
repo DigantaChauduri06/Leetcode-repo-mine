5fbe7cd1e6ec25a245a752211da32de265f8937b
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-26 21:31:27 +0530

    Time: 1223 ms (28.09%), Space: 27.5 MB (54.29%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -4,13 +4,9 @@ class Solution:
         mxLen = 0
         while (si < ei):
             cur = (ei - si) * min(height[si], height[ei])
-            # print(si, ei)
             mxLen = max(mxLen, cur)
             if height[si] > height[ei]:
                 ei-=1
             else:
                 si+=1
         return mxLen
-        
-        
-# [1,8,6,2,5,4,8,3,7]
