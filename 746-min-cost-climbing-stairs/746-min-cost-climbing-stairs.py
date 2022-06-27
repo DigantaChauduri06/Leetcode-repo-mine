5c26ef9241332a9761a6af3c8c36e5ec5087727a
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-27 09:06:56 +0530

    Time: 146 ms (5.09%), Space: 13.9 MB (75.85%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,12 +1,30 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        def helper(idx, track = {}):
-            if idx >= len(cost)-2:
-                return cost[idx]
-            if idx in track:
-                return track[idx]
-            ans = min(helper(idx+2), helper(idx+1)) + cost[idx]
-            track[idx] = ans
-            return ans
+        if len(cost) <= 2:
+            return cost[0] if len(cost) == 1 else min(cost[0], cost[1])
+        n = len(cost)
+        dp = [0] * (n+1)
+        dp[0],dp[1] = cost[0],cost[1]
         
-        return min(helper(0), helper(1)) 
+        for i in range(2, n):
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+    
+        return min(dp[n-1], dp[n-2])
+    
+    
+    
+    
+    
+    
+    
+    
+#      def helper(idx, track = {}):
+#             if idx >= len(cost)-2:
+#                 return cost[idx]
+#             if idx in track:
+#                 return track[idx]
+#             ans = min(helper(idx+2), helper(idx+1)) + cost[idx]
+#             track[idx] = ans
+#             return ans
+        
+#         return min(helper(0), helper(1)) 
