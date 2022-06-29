c1e37b2be9a4459db565dd18a5f56c61fc283b47
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-30 00:17:17 +0530

    Time: 3880 ms (19.41%), Space: 14.6 MB (72.42%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,37 +1,21 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        #find the sum of the array
-        checkSum = sum(nums)
-        
-        #if the sum is odd, we can't partition it into two sets
-        if checkSum%2 != 0:
+        totalSum = sum(nums)
+        if totalSum % 2 != 0:
             return False
-        
-        #run a cansum for the array
-        memo = {}
-        return self.canSum(nums, checkSum/2, memo)
-    
-    
-    
-    def canSum(self, nums, req, memo):
-        #return memoized value if present
-        if req in memo:
-            return memo[req]
-        #if required value < 0, return False
-        if req < 0:
-            return False
-        #if required value = 0, return True
-        if req == 0:
-            return True
-        
-        for index, num in enumerate(nums):
-            #calculate the remainder
-            remainder = req - num
-            
-            #check for this remainder in the remaining array
-            arr = [*nums[:index], *nums[index+1:]]
-            if self.canSum(arr, remainder, memo) == True:
-                memo[req] = True
+        currentTarget = totalSum // 2
+        dp = [[-1] * (currentTarget + 20)] * len(nums)
+        def dfs(i, tsum = currentTarget):
+            if i >= len(nums):
+                return False if tsum != 0 else True
+            if tsum == 0:
                 return True
-        memo[req] = False
-        return False
+            if tsum < 0:
+                return False
+            if dp[i][tsum] != -1:
+                return dp[i][tsum]
+            op1 = dfs(i+1, tsum - nums[i])
+            op2 = dfs(i+1, tsum)
+            dp[i][tsum] = op1 or op2
+            return dp[i][tsum]
+        return dfs(0)
