32e2ba8f3928be7fafd3c63418ec952d59a33f91
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-06-29 19:23:03 +0530

    Time: 65 ms (39.74%), Space: 14 MB (45.34%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,13 +1,16 @@
+# https://leetcode.com/problems/word-break/discuss/1455100/Python-3-solutions%3A-Top-down-DP-Bottom-up-DP-then-Optimised-with-Trie-Clean-and-Concise
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n+1)
+        dp[n] = True
         wordSet = set(wordDict)
-        @lru_cache(None)
-        def dfs(i):
-            if i == len(s):
-                return True
-            for ei in range(i+1, len(s)+1):
-                word = s[i: ei]
-                if word in wordSet and dfs(ei):
-                    return True
-            return False
-        return dfs(0)
+        # we are marking each and every word end as True if they found in wordSet
+        # and checking the first letter that we can potentially find or not
+        for i in range(n-1, -1, -1):
+            for j in range(i+1, n+1):
+                word = s[i:j]
+                if dp[j] and word in wordSet:
+                    dp[i] = True
+                    break
+        return dp[0]
