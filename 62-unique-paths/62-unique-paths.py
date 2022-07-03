ab514aa00def8c76ca830eca643a37b334b14ef5
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-03 20:30:11 +0530

    Time: 61 ms (18.85%), Space: 13.9 MB (33.16%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,13 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        if m == n == 1:
-            return 1
-        dp = [[0] * (n)] * (m) 
-        
-        for i in range(0, n-1):
-            dp[m-1][i] = 1
-        for i in range(0, m-1):
-            dp[i][n-1] = 1
+        dp = [[1] * (n)] * (m) 
         for i in range(m-2, -1,-1):
             for j in range(n-2, -1,-1):
                 dp[i][j] = dp[i+1][j] + dp[i][j+1] 
