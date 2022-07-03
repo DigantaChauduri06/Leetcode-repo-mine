e734302cee918eb6c32e6626bef384230a0184f8
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-03 20:18:49 +0530

    Time: 33 ms (90.76%), Space: 13.8 MB (73.63%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,16 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = {}
-        def helper(i, j):
-            if not i in range(m) or not j in range(n):
-                return 0
-            if i == m - 1 and j == n - 1:
-                return 1
-            if (i, j) in dp:
-                return dp[(i, j)]
-            op1 = helper(i+1, j)
-            op2 = helper(i, j+1)
-
-            dp[(i, j)] = op1 + op2
-            return dp[(i, j)]
-        return helper(0,0)
+        if m == n == 1:
+            return 1
+        dp = [[0] * (n)] * (m) 
+        
+        for i in range(0, n-1):
+            dp[m-1][i] = 1
+        for i in range(0, m-1):
+            dp[i][n-1] = 1
+        for i in range(m-2, -1,-1):
+            for j in range(n-2, -1,-1):
+                dp[i][j] = dp[i+1][j] + dp[i][j+1] 
+        
+        return dp[0][0]
