900335aa66d4d062fa23b7b915f96c3799511b10
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-06 08:36:32 +0530

    Time: 53 ms (48.24%), Space: 13.9 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -6,7 +6,6 @@ class Solution:
         
         for i in range(1, n+1):
             cur = prev1 + prev2
-            prev2 = prev1
-            prev1 = cur
+            prev2, prev1 = prev1, cur
             
         return prev1
