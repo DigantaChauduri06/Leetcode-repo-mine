7789be414ec65d7ead6e58c3e213d43c4987c959
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-07 13:10:50 +0530

    Added solution - LeetHub

--- Power Set - GFG/power-set.py
+++ Power Set - GFG/power-set.py
@@ -1,21 +1,23 @@
 #User function Template for python3
 class Solution:
-	def AllPossibleStrings(self, s):
-		# Code here
-            def powerSets(s):
-                if len(s) == 0:
-                    return [""]
-                smallerProblem = powerSets(s[1:])
-                ans = []
-                for st in smallerProblem:
-                    ans.append(st)
-                for i in range(len(ans)):
-                    ans.append(s[0] + ans[i])
-                return ans
-            res	= powerSets(s)
-            res.sort()
-            res.pop(0)
+    def AllPossibleStrings(self, s):
+        # Code here
+        temp, res = "", []
+        def powerSets(i = 0):
+            nonlocal temp
+            if i == len(s):
+                res.append(temp[:])
+                return res
+            # Choose
+            temp += s[i]
+            powerSets(i+1)
+            temp = temp[:-1]
+            powerSets(i+1)
             return res
+        res	= powerSets()
+        res.sort()
+        res.pop(0)
+        return res
 
 #{ 
 #  Driver Code Starts
