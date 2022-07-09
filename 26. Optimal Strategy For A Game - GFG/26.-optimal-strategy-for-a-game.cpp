7e9e50230a51bba8cb358a462e4f61f7bd34528f
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-09 13:32:15 +0530

    Added solution - LeetHub

--- 26. Optimal Strategy For A Game - GFG/26.-optimal-strategy-for-a-game.cpp
+++ 26. Optimal Strategy For A Game - GFG/26.-optimal-strategy-for-a-game.cpp
@@ -6,24 +6,22 @@ using namespace std;
 
 //Function to find the maximum possible amount of money we can win.
 class Solution{
-    public:
-long long solve(int nums[], int i, int j, long long sum, vector<vector<long long>> &dp){
-        if(dp[i][j] != -1)
-            return dp[i][j];            
-        if(j == i)  return nums[i];         
-        return dp[i][j] = max(sum - solve(nums, i + 1, j, sum - nums[i], dp), sum - solve(nums, i, j - 1, sum - nums[j], dp));
-    }
-    
-    long long maximumAmount(int arr[], int n){        
-        long long totSum = 0;
-        vector<vector<long long>> dp(n, vector<long long>(n , -1));
-        for(int i = 0; i < n; i++){
-            totSum += arr[i];
-        }
-        return solve(arr, 0 , n - 1, totSum,dp);
-    }
+   public:
+   long long maximumAmount(int arr[], int n){
+       // Your code here
+       int F[n][n];
+   for (int i = 1; i < n; i++)
+       F[i-1][i] = max(arr[i-1], arr[i]);
+   for (int k = 2; k < n; k++) {
+       for (int j = k; j < n; j++) {
+           int i = j-k, f1 = F[i+1][j], f2 = F[i][j-1];
+           if (k % 2) F[i][j]=max(arr[i]+f1,arr[j]+f2);
+           else F[i][j]=min(f1,f2);
+       }
+   }
+   return F[0][n-1];
+}
 };
-
 // { Driver Code Starts.
 int main() 
 {
