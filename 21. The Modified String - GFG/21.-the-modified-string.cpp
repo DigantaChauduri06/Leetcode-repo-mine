b82a924cb3dc027bb4e48b2a6ef0ac42ab3ae1a7
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-09 13:56:45 +0530

    Added solution - LeetHub

---
+++ 21. The Modified String - GFG/21.-the-modified-string.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+#define br char xx; cin>>xx; cout<<xx<<endl;
+#define lli long long int
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find minimum number of characters which Ishaan must insert  
+    //such that string doesn't have three consecutive same characters.
+    int modified (string a)
+    {
+int count=0;
+       for(int i=1;i<a.length()-1;i++){
+           if(a[i]==a[i-1] && a[i+1]==a[i]){
+               count++;
+               i++;
+           }
+       }
+       return count;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    string a;
+    while(t--){
+        cin>>a;
+        Solution obj;
+        cout<<obj.modified(a)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
