1e3112685115dfafd02706840fba6318898de78d
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-09 13:09:57 +0530

    Added solution - LeetHub

---
+++ 25. Foldable Binary Tree - GFG/25.-foldable-binary-tree.cpp
@@ -0,0 +1,142 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+
+
+/* A binary tree node has data, pointer to left child
+and a pointer to right child */
+/*struct node
+{
+    int data;
+    struct node* left;
+    struct node* right;
+    
+    node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+//Function to check whether a binary tree is foldable or not.
+bool checkSubtrees(Node* st1, Node* st2)
+{
+    // is both subtrees are NULL, then return true;
+    if(st1 == NULL && st2 == NULL)
+        return true;
+    // if just one is NULL, return false
+    if(st1 == NULL || st2 == NULL)
+        return false;
+    
+    // recursive compare left subtree of st1 and right subtree of st2
+    // along with right subtree of st1 and left subtree of st2;
+    return checkSubtrees(st1->left, st2->right) && checkSubtrees(st1->right, st2->left);
+}
+
+//Function to check whether a binary tree is foldable or not.
+bool IsFoldable(Node* root)
+{
+    if(root == NULL)
+        return true;
+    return checkSubtrees(root->left, root->right);
+}
+
+// { Driver Code Starts.
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        if (IsFoldable(root)) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+
+    }
+
+
+    return 0;
+}  // } Driver Code Ends
