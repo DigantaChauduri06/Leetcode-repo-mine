94202f1674bf6936f612d02d9725a7f73dab8730
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-09 12:26:07 +0530

    Time: 79 ms (5.38%), Space: 13.8 MB (77.62%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+            res, temp = [],[]
+            candidates = [((i%9)+1) for i in range(n)]
+            candidates.sort()
+            # print(candidates)
+            def helper(i, tar):
+                if i == len(candidates):
+                    if tar == 0 and len(temp) == k:
+                        res.append(temp[:])
+                    return
+                if len(temp) > k:
+                    return
+                ele_need = k - len(temp)
+                ele_left = n - i + 1
+                if ele_need > ele_left:
+                    return
+                if tar-candidates[i] >= 0:
+                    tar = tar-candidates[i]
+                    temp.append(candidates[i])
+                    while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+                        i+=1    
+                    helper(i+1, tar)
+                    temp.pop()
+                    tar = tar+candidates[i]
+                while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+                    i+=1
+                helper(i+1, tar)
+            helper(0, n)
+            return res
