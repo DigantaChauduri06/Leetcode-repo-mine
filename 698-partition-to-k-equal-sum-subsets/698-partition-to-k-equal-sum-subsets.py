36d358191a48e97f8a7c36ec310bba1e8949500e
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-11 13:28:19 +0530

    Time: 538 ms (49.54%), Space: 14.1 MB (58.84%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -0,0 +1,39 @@
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        total = sum(nums)
+        mx = max(nums)
+        reqSum = total // k
+        if total % k != 0 or reqSum < mx:
+            return False
+        picked = set()
+        nums.sort()
+        def helper(i, cur, k):
+            if k == 0:
+                return True
+            if cur == reqSum:
+                return helper(0, 0, k-1)
+            if cur > reqSum:
+                return False
+            if i >= len(nums):
+                return False
+            if i in picked:
+                return helper(i+1, cur, k)
+            picked.add(i)
+            op1 = helper(i+1, cur + nums[i], k)
+            if op1:
+                return True
+            picked.remove(i)
+            while i+1 < len(nums) and nums[i] == nums[i+1]:
+                i+=1
+            op2 = helper(i+1, cur, k)
+            
+            if op2:
+                return True
+            return op1 or op2
+        return helper(0, 0, k)
+    
+    
+    
+###################
+#       O(k*2^n)
+####################
