8d7143db034e5769f5a15946d9a01b5e5079e78c
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-11 20:14:49 +0530

    Time: 227 ms (36.26%), Space: 14 MB (39.42%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -6,22 +6,20 @@
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
         mxLen = 0
-        track, track2 = set(), set()
+        track = set()
         def helper(i, tmp):
             nonlocal mxLen
             if i == len(arr):
-                # print(tmp)
                 mxLen = max(mxLen, len(tmp))
                 return
             # non-skip
             canPick = True
             # if cur element has duplicate
-            # print(track2)
             for chars in arr[i]:
-                if chars in track2:
+                if chars in track:
                     canPick = False
-                track2.add(chars)
-            track2.clear()
+                track.add(chars)
+            track.clear()
             if canPick:
                 # if the cur element has a char which also exists in tmp
                 for chars in tmp:
@@ -34,8 +32,6 @@ class Solution:
                 track.clear()
             if canPick:
                 helper(i+1, tmp + arr[i])
-                track.clear()
-                track2.clear()
             #skip
             helper(i+1, tmp)
         
