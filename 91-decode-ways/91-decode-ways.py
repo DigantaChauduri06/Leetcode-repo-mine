552f56ad410d6a4c7bf2187c3495bb041aa8634c
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-13 08:58:28 +0530

    Time: 37 ms (84.93%), Space: 14.2 MB (10.75%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,23 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = [-1] * (len(s)+1)
         @cache
-        def numberOfWays(s):
-            if len(s) == 0:
+        def helper(i):
+            if i == len(s):
                 return 1
-            if len(s) == 1:
-                return 1 if len(s) == 1 and s[0] != '0' else 0
-            if dp[len(s)] is not -1:
-                return dp[len(s)]
-            # take single char
-            op1,op2 = 0,0
-            c = s[0:1]
-            if int(c) in range(1,10):
-                op1 = numberOfWays(s[1:])
-            #Take 2 chars
-            c = s[0:2]
-            if int(c) in range(10, 27):
-                op2 = numberOfWays(s[2:])
-            dp[len(s)] = op1 + op2
-            return dp[len(s)]
-        return numberOfWays(s)
+            op1, op2 = 0, 0
+            if (s[i] != '0' and
+                i+1 < len(s) and
+                int(s[i]+s[i+1]) in range(10,27)):
+                op1 = helper(i+2)
+            if s[i] != '0':
+                op2 = helper(i+1)
+            return op1 + op2
+        
+        return helper(0)
