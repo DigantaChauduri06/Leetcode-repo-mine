c80faf2cbb58caf60d3b4d23f77d4f26d76b4147
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-13 09:41:24 +0530

    Time: 119 ms (46.71%), Space: 14.3 MB (14.04%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.py
@@ -0,0 +1,26 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        startingColor = image[sr][sc]
+        ROW,COL = len(image), len(image[0])
+        dx = [0,0,-1,1]
+        dy = [-1,1,0,0]
+        def helper(r, c):
+            if (r < 0 or
+                c < 0 or
+                r >= ROW or
+                c >= COL or
+               image[r][c] != startingColor):
+                return
+            image[r][c] = 'X'
+            helper(r+1,c)
+            helper(r, c+1)
+            helper(r-1, c)
+            helper(r, c-1)
+        
+        helper(sr, sc)
+        for i in range(ROW):
+            for j in range(COL):
+                if image[i][j] == 'X':
+                    image[i][j] = color
+        return image
+            
