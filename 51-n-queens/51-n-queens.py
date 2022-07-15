0b41f757ebbe856efae74857d79a313eaae9758d
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-15 21:34:17 +0530

    Time: 170 ms (22.13%), Space: 14.4 MB (44.03%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,27 +1,53 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        colSet, posDig, negDig = set(), set(),set()
-        board = [["."]*n for _ in range(n)]
-        ans = []
-        def backtrack(r):
-            if r == n:
-                copy = ["".join(row) for row in board]
-                ans.append(copy)
+        res = []
+        board = [["."] * n for _ in range(n)]
+        def AllPossible(i):
+            if i == n:
+                cp = ["".join(board[i]) for i in range(n)]
+                res.append(cp)
                 return
             for c in range(n):
-                if (c in colSet or
-                   (r+c) in posDig or
-                   (r-c) in negDig):
-                    continue
-                
-                board[r][c] = 'Q'
-                colSet.add(c)
-                posDig.add(r+c)
-                negDig.add(r-c)
-                backtrack(r+1)
-                board[r][c] = '.'
-                colSet.remove(c)
-                posDig.remove(r+c)
-                negDig.remove(r-c)
-        backtrack(0)
-        return ans
+                if (not inCurrentRow(i,c) and
+                    not inNegDiag(i, c) and
+                    not inPosDiag(i, c)):
+                    board[i][c] = 'Q'
+                    AllPossible(i+1)
+                    board[i][c] = '.'
+            
+        def inCurrentRow(r, c):
+            for i in range(0, r+1):
+                if board[i][c] == 'Q':
+                    return True
+            return False
+        
+        def inNegDiag(r, c): # Left portion
+            r -= 1
+            c -= 1
+            while r >= 0 and c >= 0:
+                if board[r][c] == 'Q':
+                    return True
+                r-=1
+                c-=1
+            return False
+        
+        def inPosDiag(r, c):
+            #(3,0) -> (2,1) -> (1,2)
+            r -= 1
+            c += 1
+            while r >= 0 and c < n:
+                if board[r][c] == 'Q':
+                    return True
+                r -= 1
+                c += 1
+            return False
+        
+        AllPossible(0)
+        return res
+            
+            
+            
+            
+            
+            
+            
