f4d03c6d037b88b79e3417173b6316a1c858102a
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-16 10:48:10 +0530

    Time: 55 ms (24.76%), Space: 14 MB (11.65%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,28 +1,22 @@
 class Solution:
-    def climbStairs(self, n: int, track = {}) -> int:
-        if n <= 2: 
-            return n
-        prev1,prev2 = 1,2
-        for i in range(2, n):
-            cur = prev1 + prev2
-            prev1 = prev2
-            prev2 = cur
-            
-        return prev2
+    def climbStairs(self, n: int) -> int:
+        @cache
+        def jump(i):
+            if i == n-1:
+                return 1
+            if i == n-2:
+                return 2
+            one = jump(i+1)
+            two = jump(i+2)
+            return one + two
+        return jump(0)
         
         
         
         
         
         
-        
-# if n <= 2:
-#     return n
-# if n in track:
-#     return track[n]
-# ans = self.climbStairs(n-1)+self.climbStairs(n-2)
-# track[n] = ans
-# return ans
+
         
         
         
