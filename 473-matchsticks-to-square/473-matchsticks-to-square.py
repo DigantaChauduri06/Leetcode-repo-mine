ea6093b7c64c005404fc234d1a62a7f6287c94fa
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-07-19 19:22:51 +0530

    Time: 7999 ms (9.63%), Space: 14 MB (57.39%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.py
+++ 473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -6,31 +6,31 @@ class Solution:
         tar = total // 4
         x = tar
         seen = set()
-        def combinationSum(i, tar, k, seen):
+        def combinationSum(i, tar, k):
             if k == 0:
                 return True
             if i == len(arr):
                 if tar == 0:
-                    return combinationSum(0, x, k-1, seen)
+                    return combinationSum(0, x, k-1)
                 return False
             if tar == 0:
-                return combinationSum(0, x, k-1, seen)
+                return combinationSum(0, x, k-1)
             if tar < 0:
                 return False
             if i in seen:
-                return combinationSum(i+1, tar, k, seen)
+                return combinationSum(i+1, tar, k)
             else:
                 op1, op2 = False,False
                 if tar - arr[i] >= 0:
                     seen.add(i)
-                    op1 = combinationSum(i+1, tar - arr[i], k, seen)
+                    op1 = combinationSum(i+1, tar - arr[i], k)
                     seen.remove(i)
                 while i +1 < len(arr) and arr[i] == arr[i+1]:
                     i += 1
-                op2 = combinationSum(i+1, tar, k, seen)
+                op2 = combinationSum(i+1, tar, k)
                 return op1 or op2
         arr.sort()
-        return combinationSum(0, tar, 4, seen)           
+        return combinationSum(0, tar, 4)           
                     
                     
                     
