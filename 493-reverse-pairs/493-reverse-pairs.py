0e4015322dadfe378814901f8f316a69c80cff15
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-08-06 22:23:38 +0530

    Time: 2698 ms (62.64%), Space: 21.2 MB (92.53%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.py
@@ -0,0 +1,24 @@
+class Solution:
+    def merge(self, nums, si, m, ei):
+        i,j = si,m+1
+        while i <= m and j <= ei:
+            if nums[i] > 2 * nums[j]:
+                self.count += m - i + 1
+                j+=1
+            else:
+                i+=1
+        nums[si:ei+1] = sorted(nums[si:ei+1])
+    
+    def mergeSort(self, nums, si, ei):
+        if si >= ei:
+            return
+        m = (si + ei) // 2
+        self.mergeSort(nums, si, m)
+        self.mergeSort(nums, m+1, ei)
+        self.merge(nums, si, m, ei)
+        
+    def reversePairs(self, nums: List[int]) -> int:
+        self.count = 0
+        self.mergeSort(nums, 0, len(nums)-1)
+        # print(nums)
+        return self.count
