fcea59e7fe0e8971a419679a18807b3f4aa1ee77
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-08-08 21:28:46 +0530

    Time: 177 ms (67.46%), Space: 14.8 MB (53.17%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -0,0 +1,31 @@
+class Solution:
+    def nullifyCOL(self, mat, k):
+        for i in range(self.COL):
+            mat[k][i] = 0
+    def nullifyROW(self, mat, k):
+        for i in range(self.ROW):
+            mat[i][k] = 0
+
+    def setZeroes(self, mat: List[List[int]]) -> None:
+        rowZero = mat[0][0] == 0
+        self.ROW,self.COL = len(mat),len(mat[0])
+        for i in range(self.ROW):
+            for j in range(self.COL):
+                if mat[i][j] == 0:
+                    mat[0][j] = 0
+                    if i > 0:
+                        mat[i][0] = 0
+                    else:
+                        rowZero = True
+        for i in range(1,self.ROW):
+            for j in range(1,self.COL):
+                if mat[0][j] == 0 or mat[i][0] == 0:
+                    mat[i][j] = 0
+        if mat[0][0] == 0:
+            for r in range(self.ROW):
+                mat[r][0] = 0
+        if rowZero:
+            for c in range(self.COL):
+                mat[0][c] = 0
+            
+        
