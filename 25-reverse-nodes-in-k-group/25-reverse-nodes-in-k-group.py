c9ce48d958733343685e6ea772f742e3e3901c6f
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-08-13 09:31:01 +0530

    Time: 56 ms (85.25%), Space: 15.3 MB (12.40%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
@@ -1,6 +1,5 @@
 class Solution:        
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        #! Correct the indivisible...
         if k == 1:
             return head
         dummy = ListNode()
@@ -17,14 +16,12 @@ class Solution:
                 i+=1
             if not head:
                 break
-            # tail = head.next
             nxt = head.next
             head.next = None
             prev = self.reverse(start)
             tail = prev
             if not node:
                 node = prev
-            # self.printLL(prev)
             head = nxt
             if len(stack) > 0:
                 cur = stack.pop()
@@ -32,27 +29,10 @@ class Solution:
             if not head:
                 return node
             stack.append(start)
-        # print(tail)
         while tail and tail.next:
             tail = tail.next
         tail.next = start
         return node
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     def reverse(self, head):
         after,cur,prev = None,head, None
@@ -62,8 +42,3 @@ class Solution:
             prev = cur
             cur = after
         return prev
-    def printLL(self, head):
-        while head:
-            print(head.val, end = " ")
-            head = head.next
-        print()
