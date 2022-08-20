fc76fc6569b7476c53f52cc5df777273e759be4f
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-08-21 00:13:10 +0530

    Time: 76 ms (18.12%), Space: 14.1 MB (49.70%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,18 +1,19 @@
 class Solution:
-    def subsetsWithDup(self, nums):
-        res, temp = [],[]
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans,tmp = [],[]
         nums.sort()
         def helper(i):
             if i == len(nums):
-                res.append(temp[:])
+                ans.append(tmp[:])
                 return
-            temp.append(nums[i])
+            # Take
+            tmp.append(nums[i])
             helper(i+1)
-            temp.pop()
+            tmp.pop()
+            
+            #non-take
             while i+1 < len(nums) and nums[i] == nums[i+1]:
                 i+=1
             helper(i+1)
-            return
         helper(0)
-        return res
-        
+        return ans
