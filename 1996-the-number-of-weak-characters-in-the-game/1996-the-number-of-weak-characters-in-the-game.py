e443eae6c50399312210491a9954e32bcb87893a
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-09-10 02:31:20 +0530

    Time: 5269 ms (5.02%), Space: 67.2 MB (63.15%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfWeakCharacters(self, p: List[List[int]]) -> int:
+        p.sort(key = lambda x: (x[0], -x[1]), reverse = True)
+        cnt = 0
+        maxD = -1e9
+        for _,dif in p:
+            if dif < maxD:
+                cnt += 1
+            else:
+                maxD = dif
+        return cnt
