7c000bc0675f8717483701cf197e4fc80b95e65d
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-09-14 21:52:28 +0530

    Time: 173 ms (44.01%), Space: 16.7 MB (60.78%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,23 +1,40 @@
 class Solution:
-    def courseScheduleHelper(self, crs, nodeToMap, visited,n):
-        if crs in visited:
-            return False
-        if nodeToMap[crs] == []:
-            return True
-        visited.add(crs)
-        for pre in nodeToMap[crs]:
-            if not self.courseScheduleHelper(pre, nodeToMap, visited, n):
-                return False
-        visited.remove(crs)
-        nodeToMap[crs] = []
-        return True
-        
-    def canFinish(self, n: int, p: List[List[int]]) -> bool:
-        nodeToMap = {i: [] for i in range(n+1)}
-        for st,ed in p:
-            nodeToMap[st].append(ed)
-        visited = set()
-        for crs in range(n):
-            if not self.courseScheduleHelper(crs,nodeToMap, visited, n):
-                return False
-        return True
+    def isCycleHelper(self,sv, adj, vis, recVis):
+        vis[sv] = True
+        recVis[sv] = True
+        for node in adj[sv]:
+            if vis[node]:
+                if recVis[node]:
+                    return True
+            else:
+                if self.isCycleHelper(node, adj, vis, recVis):
+                    return True
+        recVis[sv] = False
+        return False
+    def isCyclic(self, V, adj):
+        # code here
+        vis = [False for _ in range(V+1)]
+        recVis = [False for _ in range(V+1)]
+        for i in range(V):
+            if not vis[i]:
+                if self.isCycleHelper(i, adj, vis, recVis):
+                    return True
+        return False
+    def canFinish(self, n: int, pre: List[List[int]]) -> bool:
+        adj = {i: [] for i in range(n+1)}
+        for si, ei in pre:
+            adj[si].append(ei)
+        rec, vis = set(), set()
+        return not self.isCyclic(n,adj)
+        # def isCycle(node):
+        #     vis.add(node)
+        #     rec.add(node)
+        #     for nd in adj[node]:
+        #         if not nd in vis:
+        #             if isCycle(nd):
+        #                 return True
+        #         elif nd in rec:
+        #             return True
+        #     rec.remove(node)
+        #     return False
+        # return not isCycle(0)
