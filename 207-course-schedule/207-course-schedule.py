5b9211878e02c253cc734f0ba4e0c948ab1b5a4c
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-09-14 21:54:26 +0530

    Time: 212 ms (21.93%), Space: 17.6 MB (7.24%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,40 +1,22 @@
 class Solution:
-    def isCycleHelper(self,sv, adj, vis, recVis):
-        vis[sv] = True
-        recVis[sv] = True
-        for node in adj[sv]:
-            if vis[node]:
-                if recVis[node]:
-                    return True
-            else:
-                if self.isCycleHelper(node, adj, vis, recVis):
-                    return True
-        recVis[sv] = False
-        return False
-    def isCyclic(self, V, adj):
-        # code here
-        vis = [False for _ in range(V+1)]
-        recVis = [False for _ in range(V+1)]
-        for i in range(V):
-            if not vis[i]:
-                if self.isCycleHelper(i, adj, vis, recVis):
-                    return True
-        return False
     def canFinish(self, n: int, pre: List[List[int]]) -> bool:
         adj = {i: [] for i in range(n+1)}
         for si, ei in pre:
             adj[si].append(ei)
         rec, vis = set(), set()
-        return not self.isCyclic(n,adj)
-        # def isCycle(node):
-        #     vis.add(node)
-        #     rec.add(node)
-        #     for nd in adj[node]:
-        #         if not nd in vis:
-        #             if isCycle(nd):
-        #                 return True
-        #         elif nd in rec:
-        #             return True
-        #     rec.remove(node)
-        #     return False
-        # return not isCycle(0)
+        def isCycle(node):
+            vis.add(node)
+            rec.add(node)
+            for nd in adj[node]:
+                if not nd in vis:
+                    if isCycle(nd):
+                        return True
+                elif nd in rec:
+                    return True
+            rec.remove(node)
+            return False
+        for i in range(n):
+            if isCycle(i):
+                return False
+        return True
+        
