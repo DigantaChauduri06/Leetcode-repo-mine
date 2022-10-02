bad9b74f0b71a6e996ead6fac24034e5e00c72bd
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-10-02 12:21:07 +0530

    Time: 55 ms (39.41%), Space: 14 MB (17.06%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,16 +1,30 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack)-len(needle)+1):
-            if haystack[i] == needle[0]:
-                isFound = True
-                j = i
-                k = 0
-                while k < len(needle) and j < len(haystack):
-                    if needle[k] != haystack[j]:
-                        isFound = False
-                        break
-                    k+=1
-                    j+=1
-                if isFound:
-                    return i
+        if needle == "":
+            return 0
+        lps = [0 for _ in range(len(needle))]
+        i,prevLPS = 1, 0
+        while i < len(needle):
+            if needle[i] == needle[prevLPS]:
+                lps[i] = prevLPS + 1
+                i+=1
+                prevLPS+=1
+            else:
+                if prevLPS == 0:
+                    lps[i] = 0
+                    i+=1
+                else:
+                    prevLPS = lps[prevLPS - 1]
+                    
+        i,j = 0, 0
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i,j = i+1,j+1
+            else:
+                if j == 0:
+                    i+=1
+                else:
+                    j = lps[j-1]
+            if j == len(needle):
+                return i - len(needle)
         return -1
