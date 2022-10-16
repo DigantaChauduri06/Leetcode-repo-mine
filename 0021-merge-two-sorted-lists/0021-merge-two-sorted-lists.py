08d97a31056a205f5089a73321e71b3bee1c109b
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-10-16 18:42:25 +0530

    Time: 80 ms (20.75%), Space: 13.9 MB (32.58%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -3,15 +3,37 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+"""
+def mergeTwoLists(self, h1: Optional[ListNode], h2: Optional[ListNode]) -> Optional[ListNode]:
+    if not h1 and not h2:
+        return None
+    if not h1 or not h2:
+        return h1 if not h2 else h2
+    if h2.val < h1.val:
+        h2.next = self.mergeTwoLists(h1, h2.next)
+        return h2
+    else:
+        h1.next = self.mergeTwoLists(h1.next, h2)
+        return h1
+
+"""
+
+
 class Solution:
     def mergeTwoLists(self, h1: Optional[ListNode], h2: Optional[ListNode]) -> Optional[ListNode]:
         if not h1 and not h2:
             return None
         if not h1 or not h2:
             return h1 if not h2 else h2
-        if h2.val < h1.val:
-            h2.next = self.mergeTwoLists(h1, h2.next)
-            return h2
-        else:
-            h1.next = self.mergeTwoLists(h1.next, h2)
-            return h1
+        nn = ListNode(0)
+        dummy = nn
+        while h1 and h2:
+            if h1.val < h2.val:
+                nn.next = h1
+                h1 = h1.next
+            else:
+                nn.next = h2
+                h2 = h2.next
+            nn = nn.next
+        nn.next = h1 or h2
+        return dummy.next
