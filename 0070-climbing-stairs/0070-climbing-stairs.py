695fbeaf5ffe69d185d0ab7aa9aff1fef29c2643
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-10-17 10:45:22 +0530

    Time: 65 ms (11.59%), Space: 13.9 MB (57.10%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         dp = {}
-        def helper(n):
-            if n <= 2:
-                return n
-            if n in dp:
-                return dp[n]
-            dp[n] =  helper(n-1) + helper(n-2)
-            return dp[n]
+        def helper(i):
+            if i <= 2:
+                return i
+            if i in dp:
+                return dp[i]
+            dp[i] = helper(i-1) + helper(i-2)
+            return dp[i]
         return helper(n)
