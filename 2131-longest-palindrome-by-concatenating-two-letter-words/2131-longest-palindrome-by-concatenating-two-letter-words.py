4646d4b54c8c7b8276c6ddf4c429c50fe7fa165c
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2022-11-03 13:17:00 +0530

    Time: 3914 ms (13.37%), Space: 38.3 MB (86.56%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -0,0 +1,33 @@
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        mpp = defaultdict(lambda : 0)
+        cnt = 0
+        take = set()
+        for word in words:
+            mpp[word]+=1
+        for word in words:
+            if word in mpp and word[::-1] in mpp and mpp[word] != 0  and mpp[word[::-1]] != 0:
+                if word[0] == word[1]:
+                    if mpp[word] > 1:
+                        cnt += 4
+                        mpp[word] -= 2
+                else:
+                    cnt += 4
+                    mpp[word] -= 1
+                    mpp[word[::-1]] -= 1
+                    # take.add(word)
+                    # take.add(word[::-1])
+
+        for key in mpp:
+            if mpp[key] >= 1:
+                if key[0] == key[1]:
+                    cnt += 2
+                    break
+        return cnt
+# ["ab","ty","yt","lc","cl","ab"]
+
+"""
+lctyabytcl
+
+
+"""
