30ad1d996a360b78437e4f46c9f2eace26591db7
Diganta Chaudhuri <73929709+DigantaChauduri06@users.noreply.github.com>
2023-02-05 11:37:48 +0530

    Time: 60 ms (21.99%), Space: 12 MB (18.55%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -10,14 +10,9 @@ public:
             v1[s[i]]++;
             v2[p[i]]++;
         }
-        // if (v1 == v2) ans.push_back(0);
         int j = 0;
         while (i < sn) {
             if (v1 == v2) ans.push_back(j);
-            // for (auto &p : v1) {
-            //     cout << p.first << " " << p.second << endl;
-            // }
-            // cout << endl;
             v1[s[i++]]++;
             v1[s[j]]--;
             if (v1[s[j]] == 0) 
